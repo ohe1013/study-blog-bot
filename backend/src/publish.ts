611da6bb4c634c3e587150ef3c0d9c5f18809be6
OH HYUN KEUN <dhgusrms23@naver.com>
2025-06-16 13:24:51 +0900

    goto 로직 변경

--- backend/src/publish.ts
+++ backend/src/publish.ts
@@ -244,7 +244,8 @@ async function publishBlog() {
   if (!isLoggedIn) {
     await login(page);
     await page.goto(`https://blog.naver.com/${BLOG_ID}`, {
-      waitUntil: "networkidle2",
+      waitUntil: "domcontentloaded", // 최소한 HTML 파싱까지만 기다림
+      timeout: 60000,
     });
     console.log("블로그 접속완료");
   } else {
